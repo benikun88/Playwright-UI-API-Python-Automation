fa4411c67c2cc443227c64752a15e8fdd6f5946a
Beni kun <benikun88@gmail.com>
2024-04-24 12:05:44 +0300

    init's COMMIT

--- pages/common_page.py
+++ pages/common_page.py
@@ -4,7 +4,8 @@ from playwright.sync_api import Page
 class CommonPage:
     def __init__(self, page: Page):
         self.page = page
-        self.sign_in_button = page.locator(".nav-link[data-test='nav-sign-in']")
+        self.sign_in_button = page.locator("data-test=nav-sign-in")
+        self.categories_drop_list = page.locator("data-test=nav-categories")
         self.mini_cart_button = page.locator("data-test=cart-quantity")
         # checkout flow
         self.proceed_to_checkout_button = page.locator("aw-wizard-step[steptitle='Cart'] button[type='button']")
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,11 +4,9 @@ from playwright.sync_api import Page
 class CommonPage:
     def __init__(self, page: Page):
         self.page = page
-        self.product_title = page.locator(".product-title")
-        self.quantity_field = page.locator("input[type='number']")
-        self.price = page.locator("tbody tr td:nth-child(4) span:nth-child(1)")
+        self.sort_list = page.locator("select[aria-label='sort']")
+        self.search_field = page.locator("data-test=search-query")
+        self.search_submit = page.locator("data-test=search-submit")
         self.total_price = page.locator("tbody tr td:nth-child(4) span:nth-child(1)")
         self.remove_from_cart_button = page.locator(".btn.btn-danger")
 
-    def change_quantity(self, quantity: str):
-        self.quantity_field.fill(quantity)
