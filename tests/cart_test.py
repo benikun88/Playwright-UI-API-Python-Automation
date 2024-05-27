fc4fdf0294c7f962d9bdd2e67e72951dff1c2d2e
Beni kun <benikun88@gmail.com>
2024-05-27 23:47:07 +0300

    cart test

--- tests/cart_test.py
+++ tests/cart_test.py
@@ -2,6 +2,8 @@ import allure
 import pytest
 import time  # Don't forget to import the time module!
 from playwright.sync_api import Page, expect
+from utilities.helper import HelperCommon
+from configs.config_cart import PRODUCT_QUANTITY
 from pages.common_page import CommonPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
@@ -14,9 +16,30 @@ class TestCart:
     def setup_cart_test(self):
         self.main_page = MainPage(self.page)
         self.main_page.select_item_from_search("Pliers")
-        self.product_page=ProductPage(self.page)
+        self.product_page = ProductPage(self.page)
         self.product_page.add_to_cart()
 
     def test_success_add_to_cart(self):
-        self.common_page=CommonPage(self.page)
+        self.common_page = CommonPage(self.page)
         expect(self.common_page.add_to_cart_successes_toast).to_have_text("Product added to shopping cart.")
+
+    def test_check_price_on_cart(self):
+        self.common_page = CommonPage(self.page)
+        self.cart_page = self.common_page.click_cart_button()
+        print(self.cart_page.price.text_content())
+        print(self.cart_page.total_price.text_content())
+        assert self.cart_page.price.text_content() == self.cart_page.total_price.text_content()
+
+    def test_check_price_multi_on_cart(self):
+        helper = HelperCommon()
+        self.common_page = CommonPage(self.page)
+        self.cart_page = self.common_page.click_cart_button()
+        self.cart_page.change_quantity(PRODUCT_QUANTITY)
+        time.sleep(4)
+        assert helper.convert_and_multiply_price(self.cart_page.price.text_content(), int(PRODUCT_QUANTITY)) == self.cart_page.total_price.text_content()
+
+    def test_success_remove_from_cart(self):
+        self.common_page = CommonPage(self.page)
+        self.cart_page = self.common_page.click_cart_button()
+        self.cart_page.remove_from_cart()
+        expect(self.cart_page.product_title).to_be_hidden(3)
--- utilities/helper.py
+++ utilities/helper.py
@@ -8,3 +8,15 @@ class HelperCommon:
     def generate_random_email(self):
         """Generate a random email address using faker."""
         return self.fake.email()
+
+    def convert_and_multiply_price(self, price_string, multiplier):
+        """
+        Convert a price string to a float, multiply it, and return a formatted string with a dollar sign.
+
+        :param price_string: The price string to be converted (e.g., "$12.01").
+        :param multiplier: The multiplier to apply to the price.
+        :return: The formatted price string after multiplication (e.g., "$24.02").
+        """
+        price_number = float(price_string.replace("$", ""))
+        multiplied_price = price_number * multiplier
+        return f"${multiplied_price:.2f}"
